4a4e5df55103b3cdf2eda349e40dc3f41e131635
Seungyoun Yi <younatics@gmail.com>
2019-02-03 22:28:48 +0900

    add transition demo for test

---
+++ FlexibleViewController-Demo/DetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.swift
+//  FlexibleViewController-Demo
+//
+//  Created by Seungyoun Yi on 03/02/2019.
+//  Copyright © 2019 Seungyoun Yi. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ FlexibleViewController-Demo/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  FlexibleViewController-Demo
+//
+//  Created by Seungyoun Yi on 03/02/2019.
+//  Copyright © 2019 Seungyoun Yi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "detail")
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.backgroundColor = UIColor.orange
+        
+        return cell!
+    }
+}
