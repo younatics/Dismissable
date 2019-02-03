53b94d93b6df4f96d753293cc488312c355c948e
Seungyoun Yi <younatics@gmail.com>
2019-02-03 21:54:17 +0900

    initial commit

---
+++ FlexibleViewController/FlexibleViewController.h
@@ -0,0 +1,19 @@
+//
+//  FlexibleViewController.h
+//  FlexibleViewController
+//
+//  Created by Seungyoun Yi on 03/02/2019.
+//  Copyright © 2019 Seungyoun Yi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlexibleViewController.
+FOUNDATION_EXPORT double FlexibleViewControllerVersionNumber;
+
+//! Project version string for FlexibleViewController.
+FOUNDATION_EXPORT const unsigned char FlexibleViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlexibleViewController/PublicHeader.h>
+
+
