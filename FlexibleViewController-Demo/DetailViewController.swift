1f1b606015ed75840437b341da800841b4632287
Seungyoun Yi <younatics@gmail.com>
2019-02-04 13:13:35 +0900

    Cell 고도화

--- FlexibleViewController-Demo/DetailViewController.swift
+++ FlexibleViewController-Demo/DetailViewController.swift
@@ -9,11 +9,45 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
     }
     
     @IBAction func closeButtonClicked(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
+}
+
+extension DetailViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.row {
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "first")
+            return cell!
+        case 1:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "second")
+            return cell!
+        case 2:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "third")
+            return cell!
+
+        default:
+            return UITableViewCell()
+        }
+    }
+    
+    
+}
+
+extension DetailViewController: UITableViewDelegate {
+    
 }
